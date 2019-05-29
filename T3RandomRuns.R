a24a02a7a25ffb475f131fa185baa2f7a7da57da
Leon French <leonfrench@gmail.com>
2019-05-29 11:35:45 -0400

    Code for wider random checks

--- T3RandomRuns.R
+++ T3RandomRuns.R
@@ -24,3 +24,14 @@ ggplot(data=joined, aes(x=value)) +  geom_blank() + theme_bw() +
   geom_vline(data = joined %>% filter(ID!="random"), aes(xintercept = value, color = Team)) +
   facet_wrap(. ~ variable, nrow = 2, scales = "free") + xlab("") + ylab("")
 
+
+#pick some random runs, get max score and test if it beats the CAMH score
+random_runs <- 18 #or 5 can be used
+better_score = 0
+for (i in 1:10000) {
+  cat(".")
+  if ((random %>% filter(variable == "ADODL") %>% sample_n(random_runs) %>% arrange(-value) %>% head(1) %>% .$value) >= 0.810) {
+    better_score <- better_score + 1
+  }
+}
+print(better_score)
